883501b9bae2cb347a71a4db07c90b20e96f36e4
Nikites <rockstar.gg@mail.ru>
2023-03-02 20:55:26 +0300

    add claendar, nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:monkey_app/home.dart';
 import 'auth.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  // DBConnection().connectDB();
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyThemeApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyThemeApp extends StatelessWidget {
+  const MyThemeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
       debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+      // home: AuthPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const AuthPage(),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
